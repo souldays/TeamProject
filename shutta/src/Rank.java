5dd22b4fc437db6446d365a33a63c752b640e5cb
rjsgh134 <rjsgh134@naver.com>
2017-10-20 09:47:35 +0900

    aa

--- shutta/src/Rank.java
+++ shutta/src/Rank.java
@@ -2,14 +2,18 @@ public class Rank {
     static public void decisionRank(Player p1, Player p2){
 
         System.out.println( p1.getName()+" : "+p1.getMoney());
-        System.out.println(p1.getName()+" 승률 : "+(p1.getRecord().getPoV())+"%");
+        System.out.printf("%s 승률 : %.2f ",p1.getName(),p1.getRecord().getPoV());
+        System.out.println("% \n");
+        //System.out.println(p1.getName()+" 승률 : "+(p1.getRecord().getPoV())+"%");
         System.out.println(p1.getRecord().getWinCount()+"번 승리");
         System.out.println(p1.getRecord().getDrawCount()+"번 무승부");
         System.out.println(p1.getRecord().getLoseCount()+"번 패배");
         System.out.println();
 
         System.out.println(p2.getName()+" : "+p2.getMoney());
-        System.out.println(p2.getName()+" 승률 : "+(p2.getRecord().getPoV())+"%");
+        System.out.printf("%s 승률 : %.2f ",p2.getName(),p2.getRecord().getPoV());
+        System.out.println("% \n");
+        //System.out.println(p2.getName()+" 승률 : "+(p2.getRecord().getPoV())+"%");
         System.out.println(p2.getRecord().getWinCount()+"번 승리");
         System.out.println(p2.getRecord().getDrawCount()+"번 무승부");
         System.out.println(p2.getRecord().getLoseCount()+"번 패배");
